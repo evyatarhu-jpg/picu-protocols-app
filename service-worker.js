d3ad4c6e3de3ac35820d1d6dd30458c1deed218c
evyatarhu-jpg <evyatarhu@gmail.com>
2026-09-25 11:14:42 +0000

    Add Bivalirudin protocol

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "picu-v10";
+const CACHE = "picu-v11";
 
 const APP_SHELL = [
   "./",
@@ -9,6 +9,7 @@ const APP_SHELL = [
   "./dka.html",
   "./bleeding.html",
   "./anticoagulation.html",
+  "./bivalirudin.html",
   "./warfarin.html",
   "./feeding.html",
   "./air-embolism.html",
